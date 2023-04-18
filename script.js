78c5ef2b848fe716b1efa9002da54c2129129fc7
Kirtan Patel <83908992+kirtanpatel2003@users.noreply.github.com>
2023-04-18 18:01:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.getElementsByTagName("section");
+  const sectionHeaders = document.getElementsByTagName("h2");
 
-\f0\fs24 \cf0 document.getElementById('revealBtn').addEventListener('click', function () \{\
-  document.getElementById('hiddenMessage').style.display = 'block';\
-\});\
-}
+  for (let i = 0; i < sectionHeaders.length; i++) {
+    sectionHeaders[i].addEventListener("click", function () {
+      sections[i].classList.toggle("hidden");
+    });
+  }
+});
