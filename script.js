1a234666c4f7a516273d79d8dba39d19d41e4078
Kirtan Patel <83908992+kirtanpatel2003@users.noreply.github.com>
2023-06-05 14:42:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('header nav ul li a');
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = event.target.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      const headerOffset = document.querySelector('header').offsetHeight;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - headerOffset;
+
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    });
+  });
+});
