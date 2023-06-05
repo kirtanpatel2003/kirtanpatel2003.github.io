3ca5b249a5e2bb787fd47e6b2e8b02c3f0bc0195
Kirtan Patel <83908992+kirtanpatel2003@users.noreply.github.com>
2023-06-05 11:40:05 -0500

    Update script.js
    try

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
   AOS.init();
 });
 
-//   const navLinks = document.querySelectorAll('header nav ul li a');
+  const navLinks = document.querySelectorAll('header nav ul li a');
 
-//   navLinks.forEach((link) => {
-//     link.addEventListener('click', (event) => {
-//       event.preventDefault();
-//       const targetId = event.target.getAttribute('href');
-//       const targetElement = document.querySelector(targetId);
-//       const headerOffset = document.querySelector('header').offsetHeight;
-//       const elementPosition = targetElement.getBoundingClientRect().top;
-//       const offsetPosition = elementPosition - headerOffset;
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = event.target.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      const headerOffset = document.querySelector('header').offsetHeight;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - headerOffset;
 
-//       window.scrollBy({
-//         top: offsetPosition,
-//         behavior: 'smooth',
-//       });
-//     });
-//   });
-// });
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    });
+  });
+});
