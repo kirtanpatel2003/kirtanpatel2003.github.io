e26c00e85c014fc5337e33d1b2533618ab7a3803
Kirtan Patel <83908992+kirtanpatel2003@users.noreply.github.com>
2023-06-05 13:29:31 -0500

    Update script.js
    trails

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// script.js
 document.addEventListener("DOMContentLoaded", function(event) {
   AOS.init();
 
   const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll("header nav ul li a");
+  const navLinks = document.querySelectorAll(".sidebar nav ul li a");
 
-  function setActiveSection() {
+  function setActiveLink() {
     const scrollPosition = window.pageYOffset;
 
     sections.forEach(function(section) {
@@ -12,19 +13,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
       const sectionHeight = section.offsetHeight;
 
       if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-        section.classList.add("active");
         navLinks.forEach(function(link) {
           if (link.getAttribute("href") === "#" + section.id) {
-            link.classList.add("active");
+            link.classList.add("active-link");
           } else {
-            link.classList.remove("active");
+            link.classList.remove("active-link");
           }
         });
-      } else {
-        section.classList.remove("active");
       }
     });
   }
 
-  window.addEventListener("scroll", setActiveSection);
+  // Call the function immediately
+  setActiveLink();
+  // And also call it when the user scrolls
+  window.addEventListener("scroll", setActiveLink);
 });
