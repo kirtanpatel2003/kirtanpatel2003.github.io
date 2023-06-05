f84bb2c685397db5b60e0b8298c1628e4d343313
Kirtan Patel <83908992+kirtanpatel2003@users.noreply.github.com>
2023-06-05 14:25:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        var hashval = this.getAttribute('href');
+        var target = document.querySelector(hashval);
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
         });
+        history.pushState(null, null, hashval);
     });
 });
 
