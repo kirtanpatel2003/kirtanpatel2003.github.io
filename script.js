a5dedc1b995bb18476b15e765c67dd7f56563f7e
Kirtan Patel <83908992+kirtanpatel2003@users.noreply.github.com>
2023-06-05 12:46:08 -0500

    Update script.js
    scroll

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) {
   AOS.init();
 
-  const navLinks = document.querySelectorAll('nav ul li a');
+  const sections = document.querySelectorAll("section");
+
+  window.addEventListener("scroll", () => {
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const scrollPosition = window.pageYOffset;
+
+      if (
+        scrollPosition >= sectionTop - sectionHeight * 0.5 &&
+        scrollPosition < sectionTop + sectionHeight * 0.5
+      ) {
+        section.classList.add("active");
+      } else {
+        section.classList.remove("active");
+      }
+    });
+  });
+
+  const navLinks = document.querySelectorAll("nav ul li a");
 
   navLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
+    link.addEventListener("click", (event) => {
       event.preventDefault();
-      const targetId = event.target.getAttribute('href');
+      const targetId = event.target.getAttribute("href");
       const targetElement = document.querySelector(targetId);
-      const headerOffset = document.querySelector('.header-content').offsetHeight;
+      const headerOffset = document.querySelector(".header-content").offsetHeight;
       const elementPosition = targetElement.offsetTop;
       const offsetPosition = elementPosition - headerOffset;
 
       window.scrollTo({
         top: offsetPosition,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
     });
   });
