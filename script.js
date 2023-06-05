b1f2d2ab43bd321d68f57249e9bbb1fa80b67258
Kirtan Patel <83908992+kirtanpatel2003@users.noreply.github.com>
2023-06-05 13:01:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  AOS.init();
+
   const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll("header nav ul li a");
 
