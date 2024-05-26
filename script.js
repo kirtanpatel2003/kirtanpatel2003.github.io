05953c7264bd483117bcbed5cd650393217f6554
Kirtan Patel <official.kirtan13@gmail.com>
2024-05-26 18:10:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,9 @@ window.onload = function() {
     allKeys.forEach(key => {
         key.style.opacity = 1;
     });
-
     window.addEventListener('scroll', function() {
-        if (!showSpecial && window.scrollY > 50) {
-            specialKeys.forEach(key => {
-                key.style.opacity = 1;
-            });
-            showSpecial = true;
-        } else if (showSpecial && window.scrollY > 300) {
+        const name = document.querySelector('.name');
+        if (window.scrollY > 100) { // Adjust as needed
             name.style.opacity = 1;
         }
     });
