007d324b0688fec00c4f18a7d358725fe9e01f01
Kirtan Patel <official.kirtan13@gmail.com>
2024-05-26 16:39:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const navLinks = document.querySelectorAll('header nav ul li a');
+const text = "Hello! I'm Kirtan. Welcome to my portfolio site.";
+const typingText = document.getElementById('typewriter-text');
+let index = 0;
 
-  navLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const targetId = event.target.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      const headerOffset = document.querySelector('header').offsetHeight;
-      const elementPosition = targetElement.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - headerOffset;
+function typeWriter() {
+    if (index < text.length) {
+        typingText.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 150);
+    }
+}
 
-      window.scrollBy({
-        top: offsetPosition,
-        behavior: 'smooth',
-      });
-    });
-  });
-});
-
-
-// Add this to your JavaScript
-document.addEventListener('DOMContentLoaded', function () {
-  const sections = document.querySelectorAll('main section');
-
-  function checkScroll() {
-    const triggerBottom = window.innerHeight / 2;
-
-    sections.forEach(section => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const sectionBottom = section.getBoundingClientRect().bottom;
-
-      if (sectionTop < triggerBottom && sectionBottom > triggerBottom) {
-        section.classList.add('visible');
-      } else {
-        section.classList.remove('visible');
-      }
-    });
-  }
-
-  window.addEventListener('scroll', checkScroll);
-  checkScroll();
-});
+typeWriter();
