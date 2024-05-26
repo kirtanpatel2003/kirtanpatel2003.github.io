f766b32e551b18bb9efd6fcec77e3cd25af1a7ce
Kirtan Patel <official.kirtan13@gmail.com>
2024-05-26 18:21:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// window.onload = function() {
-//     const allKeys = document.querySelectorAll('.key');
-//     const specialKeys = document.querySelectorAll('.special');
-//     const name = document.querySelector('.name');
-//     let showSpecial = false;
-
-//     allKeys.forEach(key => {
-//         key.style.opacity = 1;
-//     });
-//     window.addEventListener('load', function() {
-//         const name = document.querySelector('.name');
-//         name.style.opacity = 0; // Ensure name is hidden on load
-    
-//         window.addEventListener('scroll', function() {
-//             if (window.scrollY > 300) {  // Adjust this value based on where you want the effect to trigger
-//                 name.style.opacity = 1;
-//             } else {
-//                 name.style.opacity = 0; // Hide the name when not at the scroll position
-//             }
-//         });
-//     });
-// };
 window.onload = function() {
     const allKeys = document.querySelectorAll('.key');
     const name = document.querySelector('.name');
