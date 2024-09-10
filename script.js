c3f7229c76bec4cab37c91eaae2d48ca9eb45567
Kirtan Patel <official.kirtan13@gmail.com>
2024-09-10 13:01:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ const careerElement = document.querySelector('.career');
 
 function typeWriter(text, i, callback) {
     if (i < text.length) {
-        careerElement.innerHTML = text.substring(0, i+1) + '<span class="cursor">|</span>';
+        careerElement.innerHTML = text.substring(0, i + 1);
         setTimeout(() => typeWriter(text, i + 1, callback), 100);
     } else if (typeof callback === 'function') {
         setTimeout(callback, 700); // Pause before starting the next career
     }
 }
-
 function startTyping() {
     typeWriter(careers[index], 0, () => {
         index = (index + 1) % careers.length;
@@ -19,3 +18,18 @@ function startTyping() {
 }
 
 startTyping();
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    const navbar = document.querySelector('#navbar');
+
+    if (window.scrollY > 100) {
+        document.body.classList.add('shrink');
+        navbar.classList.remove('hidden');
+        navbar.classList.add('visible');
+    } else {
+        document.body.classList.remove('shrink');
+        navbar.classList.remove('visible');
+        navbar.classList.add('hidden');
+    }
+});
